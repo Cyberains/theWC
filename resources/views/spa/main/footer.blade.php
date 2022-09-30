5cda88562b6596f18ba8c5177860491f3b648c31
itslalanchaudhary <lalan1.epic@gmail.com>
2022-09-30 16:52:18 +0530

    footer change

--- resources/views/spa/main/footer.blade.php
+++ resources/views/spa/main/footer.blade.php
@@ -35,11 +35,12 @@
                     <div class="footer-link">
                         <h6 class="footer-title">Contact Us</h6>
                         <ul>
-                            <p class="text-footer"><i class="fa fa-map-signs fw pr-1"></i> Visit : F-256 , 1st Floor , B-Block , Ansal API , Palam Corporate Plaza, Palam Vihar Gurgaon (HR), 122017</p>
-                            <li class="text-justify"><i class="fa fa-phone pr-1"></i><a href="tel:8860014004">    Call Now (+91) 88-6001-4004</a></li>
-                            <li><i class="fa fa-phone pr-1"></i><a href="tel:0124-427-8955">  Phone No : 0124-427-8955</a></li>
-                            <li><i class="fa fa-envelope pr-1"></i><a href="mailto:partner@thewomenscompany.in" target="_blank"> partner@thewomenscompany.in</a></li>
-                        </ul>
+                                <p class="text-footer"><i class="fa fa-map-signs fw pr-1"></i> Visit : F-256 , 1st Floor , B-Block , Ansal API , Palam Corporate Plaza, Palam Vihar Gurgaon (HR), 122017</p>
+                                <li class="text-justify"><i class="fa fa-phone pr-1"></i><a href="tel:8860014004">    Call Now (+91) 88-6001-4004</a></li>
+                                <li>
+                                    <i class="fa fa-phone pr-1"></i><a href="tel:0124-427-8955">  Phone No : 0124-427-8955</a></li>
+                                <li><i class="fa fa-envelope pr-1"></i><a href="mailto:partner@thewomenscompany.in" target="_blank"> partner@thewomenscompany.in</a></li>
+                            </ul>
                     </div>
                 </div>
             </div>
@@ -60,12 +61,15 @@
                     </div>
                 </div>
                 <div class="col-lg-5">
-                    <ul class="social text-center text-lg-right mt-10">
-                        <li><a href="https://www.facebook.com/earlybasketapp/"><i class="lni-facebook-filled"></i></a></li>
-                        <li><a href="#"><i class="lni-twitter-original"></i></a></li>
-                        <li><a href="https://www.instagram.com/the_womens_company" target="_blank"><i class="lni-instagram-original"></i></a></li>
-                        <li><a href="https://www.linkedin.com/company/the-womens-company" target="_blank"><i class="lni-linkedin-original"></i></a></li>
-                    </ul>
+                <ul class="social text-center text-lg-right mt-10">
+                            <li><a href="https://www.facebook.com/people/The-Womens-Company/100085780710556" target="_blank"><i class="lni-facebook-filled"></i></a></li>
+                            <li><a href="#"><i class="lni-twitter-original"></i></a></li>
+
+                            <li><a href="https://www.instagram.com/the_womens_company" target="_blank"><i class="lni-instagram-original"></i></a></li>
+                            <li><a href="https://www.linkedin.com/company/the-womens-company" target="_blank"><i class="lni-linkedin-original"></i></a></li>
+                            <li><a href="https://api.whatsapp.com/send?phone=918860014004" target="_blank"><i class="lni lni-whatsapp"></i></a></li>
+                        </ul>
+
                 </div>
             </div>
         </div>
