322f3c47d468dd03944a57b0ffdad0bacfa2919d
itslalanchaudhary <lalan1.epic@gmail.com>
2022-10-10 16:51:58 +0530

    update

--- resources/views/spa/main/navbar.blade.php
+++ resources/views/spa/main/navbar.blade.php
@@ -1,6 +1,7 @@
 @section('styles')
 <style>
 
+
 </style>
 @endsection
 
@@ -8,32 +9,25 @@
 
 
 <section id="topbar" class="topbar d-flex align-items-center">
-        <div class="container d-flex justify-content-center justify-content-md-between">
-            <div class="contact-info d-flex align-items-center"> 
-                <i class="bi bi-envelope d-flex align-items-center">
-                    <a href="mailto:contact@example.com">contact@example.com</a>
+        <div class="container-fluid d-flex justify-content-center justify-content-md-between">
+            <div class="contact-info d-none d-md-flex align-items-center float-left mr-auto"> 
+                <i class="bi bi-envelope d-flex align-items-center p-2">
+                    <a href="mailto:support@thewomenscompany.in">support@thewomenscompany.in</a>
                 </i>
                 <i class="bi bi-phone d-flex align-items-center ms-4">
-                    <span>+1 5589 55488 55</span>
+                   <a href="tel:918860014004">(+91) 88-6001-4004</a>
                 </i>
             </div>
-            <div class="social-links d-none d-md-flex align-items-center"> 
-                <a href="#" class="twitter"><i class="bi bi-twitter"></i></a> 
-                <a href="#" class="facebook"><i class="bi bi-facebook"></i></a> 
-                <a href="#" class="instagram"><i class="bi bi-instagram"></i></a> 
-                <a href="#" class="linkedin"><i class="bi bi-linkedin"></i></a>
+            <div class="social-links d-md-flex align-items-center"> 
+                <a href="#" class="twitter p-2" style="color:#fff;"><i class="bi bi-person"></i> Login</a> 
+                <a href="#" class="facebook" style="color:#fff;"><i class="bi bi-arrow-right-short"></i>Be Come a Professional</a> 
+                
             </div>
         </div>
     </section>
   <section class="header-area bg-section">
     <div class="navbar-area">
         <div class="container-fluid">
-             <!--   <div class="row">
-                        <ul class="top_header ml-auto">
-                          <li><a href="#" class="active">Login</a></li>
-                           <li><a href="#" class="">Register</a></li>
-                        </ul>
-                      </div> -->
             <div class="row">
                 <div class="col-lg-12">
                     <nav class="navbar navbar-expand-lg">
