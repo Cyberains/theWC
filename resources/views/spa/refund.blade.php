5fe399b1bcf5fb06aa5fdd1d3d05706ccb281900
itslalanchaudhary <lalan1.epic@gmail.com>
2022-10-11 16:48:40 +0530

    refrund policy

--- resources/views/spa/refund.blade.php
+++ resources/views/spa/refund.blade.php
@@ -17,90 +17,61 @@
         </div>
     </div>
 
-    <section class="py-4">
+     <section class="py-4">
         <div class="container">
             <div class="text-center pt-120">
                 <h1>Return and Refund Policy</h1>
             </div>
-            <p class="gray">Last updated: October 11, 2022</p>
-            <p>Thank you for shopping at The Women's Company website and TWC (The Women's Company) app.</p>
-            <p>If, for any reason, You are not completely satisfied with a purchase We invite You to review our policy on refunds and returns.
-            <p>The following terms are applicable for any products that You purchased with Us.</p>
-            <h1>Interpretation and Definitions</h1>
-            <h2>Interpretation</h2>
-            <p>The words of which the initial letter is capitalized have meanings defined under the following conditions. The following definitions shall have the same meaning regardless of whether they appear in singular or in plural.</p>
-            <h2>Definitions</h2>
-            <p>For the purposes of this Return and Refund Policy:</p>
-            <ul>
-                <li>
-                    <p><strong>Application</strong> means the software program provided by the Company downloaded by You on any electronic device, named TWC (The Women's Company)</p>
-                </li>
-                <li>
-                    <p><strong>Company</strong> (referred to as either &quot;the Company&quot;, &quot;We&quot;, &quot;Us&quot; or &quot;Our&quot; in this Agreement) refers to Epic Corporations Pvt Ltd, 736 LGF, Udyog Vihar Ph 5, GUrgaon.</p>
-                </li>
-                <li>
-                    <p><strong>Goods</strong> refer to the items offered for sale on the Service.</p>
-                </li>
-                <li>
-                    <p><strong>Orders</strong> mean a request by You to purchase Goods from Us.</p>
-                </li>
-                <li>
-                    <p><strong>Service</strong> refers to the Application or the Website or both.</p>
-                </li>
-                <li>
-                    <p><strong>Website</strong> refers to The Women's Company, accessible from <a href="https://thewomenscompany.in/" rel="external nofollow noopener" target="_blank">https://thewomenscompany.in/</a></p>
-                </li>
-                <li>
-                    <p><strong>You</strong> means the individual accessing or using the Service, or the company, or other legal entity on behalf of which such individual is accessing or using the Service, as applicable.</p>
-                </li>
-            </ul>
-            <h1>Your Order Cancellation Rights</h1>
-            <p>You are entitled to cancel Your Order before 30 mins to start your service without giving any reason for doing so. For any beauty product you can cancel Your Order within 7 days without giving any reason for doing so. </p>
-            <p>The deadline for cancelling an service is before 30 mins and for beauty product Order is 7 days from the date on which You received the Goods or on which a third party you have appointed, who is not the carrier, takes possession of the product delivered. </p>
-            <p>In order to exercise Your right of cancellation, You must inform Us of your decision by means of a clear statement. You can inform us of your decision by:</p>
-            <ul>
-                <li>
-                    <p>By email: support@thewomenscompany.in</p>
-                </li>
-                <li>
-                    <p>By phone number: +91-88 6001 4004</p>
-                </li>
-            </ul>
-            <p>We will reimburse You no later than 7 days from the day of cancelation of service and for goods it will be 14 days from the day of cancelation on which We receive the returned Goods. We will use the same means of payment as You used for the Order, and You will not incur any fees for such reimbursement for products INR 100+ GST will be deducted from refund in case of service bookings (as platform use charge). Within 30 mins 50% of the total cost will be deducted and rest will refund within 7 working days. </p>
-            <h1>Conditions for Returns</h1>
-            <p>In order for the services to be eligible for a cancellation before 30 mins, please make sure that:</p>
 
-            <p>In order for the Goods to be eligible for a return, please make sure that:</p>
-            <ul>
-                <li>The Goods were purchased in the last 7 days</li>
-                <li>The Goods are in the original packaging</li>
-            </ul>
-            <p>The following Goods cannot be returned:</p>
-            <ul>
-                <li>The supply of Goods made to Your specifications or clearly personalized.</li>
-                <li>The supply of Goods which according to their nature are not suitable to be returned, deteriorate rapidly or where the date of expiry is over.</li>
-                <li>The supply of Goods which are not suitable for return due to health protection or hygiene reasons and were unsealed after delivery.</li>
-                <li>The supply of Goods which are, after delivery, according to their nature, inseparably mixed with other items.</li>
-            </ul>
-            <p>We reserve the right to refuse returns of any merchandise that does not meet the above return conditions in our sole discretion.</p>
-            <p>Only regular priced Goods may be refunded. Unfortunately, Goods on sale cannot be refunded. This exclusion may not apply to You if it is not permitted by applicable law.</p>
-            <h1>Returning Goods</h1>
-            <p>You are responsible for the cost and risk of returning the Goods to Us. You should send the Goods at the following address:</p>
-            <p>LGF 736, Udyog Vihar Ph 5, Gurgaon (HR)</p>
-            <p>We cannot be held responsible for Goods damaged or lost in return shipment. Therefore, We recommend an insured and trackable mail service. We are unable to issue a refund without actual receipt of the Goods or proof of received return delivery.</p>
-            <h1>Gifts</h1>
-            <p>If the Goods were marked as a gift when purchased and then shipped directly to you, You'll receive a gift credit for the value of your return. Once the returned product is received, a gift certificate will be mailed to You.</p>
-            <p>If the Goods weren't marked as a gift when purchased, or the gift giver had the Order shipped to themselves to give it to You later, We will send the refund to the gift giver.</p>
-            <h2>Contact Us</h2>
-            <p>If you have any questions about our Returns and Refunds Policy, please contact us:</p>
-            <ul>
-                <li>
-                    <p>By email: support@thewomenscompany.in</p>
-                </li>
-                <li>
-                    <p>By phone number: +91-88 6001 4004</p>
-                </li>
-            </ul>
+            <b style="font-size: 1.6rem;">Last updated: October 11, 2022</b>
+            <p class="twc_tx">Thank you for shopping at The Women's Company website and TWC (The Women's Company) app.
+                If, for any reason, You are not completely satisfied with a purchase We invite You to review our policy on refunds and returns.<br>
+                The following terms are applicable for any products that You purchased with Us.
+            </p>
+            <h3> Interpretation and Definitions</h3>
+            <p class="twc_tx"><b>Interpretation:</b> The words of which the initial letter is capitalized have meanings defined under the following conditions. The following definitions shall have the same meaning regardless of whether they appear in singular or in plural. </p>
+            <h3>Definitions</h3>
+            <p class="twc_tx"><b>For the purposes of this Return and Refund Policy:</b> </p>
+            <p class="twc_tx"><b>Application:</b> > means the software program provided by the Company downloaded by You on any electronic device, named TWC (The Women's Company).</p>
+            <p class="twc_tx"><b>Company:</b> > (referred to as either &quot;the Company&quot;, &quot;We&quot;, &quot;Us&quot; or &quot;Our&quot; in this Agreement) refers to Epic Corporations Pvt Ltd, 736 LGF, Udyog Vihar Ph 5, GUrgaon.</p>
+            <p class="twc_tx"><b>Goods:</b> > refer to the items offered for sale on the Service.</p>
+            <p class="twc_tx"><b>Orders:</b> mean a request by You to purchase Goods from Us.</p>
+            <p class="twc_tx"><b>Service:</b> refers to the Application or the Website or both.</p>
+            <p class="twc_tx"><b>Website:</b> refers to The Women's Company, accessible from <a href="https://thewomenscompany.in/" rel="external nofollow noopener" target="_blank">https://thewomenscompany.in/</a></p>
+
+            <p class="twc_tx pl-2"><b>You:</b> means the individual accessing or using the Service, or the company, or other legal entity on behalf of which such individual is accessing or using the Service, as applicable.</p>
+
+            <h3>Your Order Cancellation Rights</h3>
+
+            <p class="twc_tx">You are entitled to cancel Your Order before 30 mins to start your service without giving any reason for doing so. For any beauty product you can cancel Your Order within 7 days without giving any reason for doing so. <br>
+                The deadline for cancelling an service is before 30 mins and for beauty product Order is 7 days from the date on which You received the Goods or on which a third party you have appointed, who is not the carrier, takes possession of the product delivered.
+            </p>
+            <p class="twc_tx"><b>In order to exercise Your right of cancellation, You must inform Us of your decision by means of a clear statement. You can inform us of your decision by:</b></p>
+
+            <p class="twc_tx"><b>By email:</b> support@thewomenscompany.in</p>
+            <p class="twc_tx"><b>By phone number: </b> +91-88 6001 4004</p>
+            <p class="twc_tx">We will reimburse You no later than 7 days from the day of cancelation of service and for goods it will be 14 days from the day of cancelation on which We receive the returned Goods. We will use the same means of payment as You used for the Order, and You will not incur any fees for such reimbursement for products INR 100+ GST will be deducted from refund in case of service bookings (as platform use charge). Within 30 mins 50% of the total cost will be deducted and rest will refund within 7 working days.</p>
+            <h3>Conditions for Returns</h3>
+            <p><b>In order for the services to be eligible for a cancellation before 30 mins, please make sure that:</b></p>
+            <p class="twc_tx"><b>In order for the Goods to be eligible for a return, please make sure that:</b></p>
+            <p class="twc_tx">The Goods were purchased in the last 7 days</p>
+            <p class="twc_tx">The Goods are in the original packaging</p>
+            <p class="twc_tx"><b>The following Goods cannot be returned:</b></p>
+            <p class="twc_tx">The supply of Goods made to Your specifications or clearly personalized.</p>
+            <p class="twc_tx">The supply of Goods which according to their nature are not suitable to be returned, deteriorate rapidly or where the date of expiry is over.</p>
+            <p class="twc_tx">The supply of Goods which are not suitable for return due to health protection or hygiene reasons and were unsealed after delivery.</p>
+            <p class="twc_tx">The supply of Goods which are, after delivery, according to their nature, inseparably mixed with other items.</p>
+            <p class="twc_tx">We reserve the right to refuse returns of any merchandise that does not meet the above return conditions in our sole discretion.<br>
+               Only regular priced Goods may be refunded. Unfortunately, Goods on sale cannot be refunded. This exclusion may not apply to You if it is not permitted by applicable law. </p>
+               <h3>Returning Goods</h3>
+            <p class="twc_tx"><b>You are responsible for the cost and risk of returning the Goods to Us. You should send the Goods at the following address:</b></p>
+            <p class="twc_tx">LGF 736, Udyog Vihar Ph 5, Gurgaon (HR)</p>
+            <p class="twc_tx">We cannot be held responsible for Goods damaged or lost in return shipment. Therefore, We recommend an insured and trackable mail service. We are unable to issue a refund without actual receipt of the Goods or proof of received return delivery.</p>
+            <h3>Gifts</h3>
+            <p class="twc_tx">If the Goods were marked as a gift when purchased and then shipped directly to you, You'll receive a gift credit for the value of your return. Once the returned product is received, a gift certificate will be mailed to You.<br>
+                If the Goods weren't marked as a gift when purchased, or the gift giver had the Order shipped to themselves to give it to You later, We will send the refund to the gift giver.
+            </p>
+           
         </div>
     </section>
 @endsection
